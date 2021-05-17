cc9cd5445b91fcec3f868241bd295c4f105a7c7e
admin <admin@macelab-server.biochem.ucl.ac.uk>
2021-05-17 20:31:13 +0100

    auto update from server

--- R/functions.R
+++ R/functions.R
@@ -262,7 +262,7 @@ wordcloud.maker <- function(freq, col, png.file){
 	wc <- wordcloud2(freq, size=size, color = col, minRotation = 0, maxRotation = pi/2,widgetsize=c(width,height))
 	html.file <- 'tmp.html'
 	saveWidget(wc,html.file,selfcontained = F)	
- 	webshot(html.file,png.file, delay =30, vwidth = width, vheight=height) %>% shrink()
+ 	webshot(html.file,png.file, delay =20, vwidth = width, vheight=height) %>% shrink()
 
 	# imagemagick
 	system(paste('magick convert ',png.file,' -gravity South -chop 0x20 -trim -resize 800x800> ',png.file,sep=''))
