0e8cce60a76869459b6bdf8bdb5f3a56c8bf5fdb
Adrian Timpson <a.timpson@ucl.ac.uk>
2022-04-27 08:34:04 +0100

    auto update from server

--- R/controller.R
+++ R/controller.R
@@ -6,17 +6,17 @@ install_phantomjs(force = TRUE)
 
 #ud_model <- udpipe_download_model(language = "english", model_dir='../tools/data')
 #ud_model <- udpipe_load_model(ud_model$file_model)
-ud_model <- udpipe_load_model('../tools/data/english-ewt-ud-2.5-191206.udpipe')
+#ud_model <- udpipe_load_model('../tools/data/english-ewt-ud-2.5-191206.udpipe')
 #-------------------------------------------------------------------------------------------
-correction.tidy()
+#correction.tidy()
 Sys.time()
-source('generate list of UPIs.R')
+#source('generate list of UPIs.R')
 Sys.time()
-source('generate wordcloud for each UPI.R')
+#source('generate wordcloud for each UPI.R')
 Sys.time()
-source('generate list of department codes.R')
+#source('generate list of department codes.R')
 Sys.time()
-source('generate wordcloud for each department.R')
+#source('generate wordcloud for each department.R')
 Sys.time()
 #-------------------------------------------------------------------------------------------
 
